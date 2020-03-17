0be480d151ad856b07967a185423a9775d6cb856
Cleverson Scharf <cleverson.scharf@solvis.com.br>
2020-03-17 14:04:30 -0300

    Botao add to cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+
+  has_many :line_itens
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence:true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -7,4 +12,14 @@ class Product < ApplicationRecord
     message: 'must be a URL for gif, JPG or PNG image'
   }
   validates :title, length:{minimum:10}, allow_blank:true
+
+  private
+
+  #ensure that there are no line itens referencing this product
+  def ensure_not_referenced_by_any_line_item
+    unless lines_items.empty?
+      errors.add(:base, 'Lines Items present')
+      throw :abort
+    end
+  end
 end
